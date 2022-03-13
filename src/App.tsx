93ff71cae96bd9419ea0797f85402e1cd069d588
ryota-09 <n.ryo950911@gmail.com>
2022-03-13 13:03:13 +0900

    テーマカラー設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "./theme/theme";
 
 function App() {
   return (
     <div className="App">
-      <h1>タイトル</h1>
+      <ChakraProvider theme={theme}>
+        <h1>タイトル</h1>
+      </ChakraProvider>
     </div>
   );
 }
